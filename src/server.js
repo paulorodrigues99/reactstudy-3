657c979cadb06ae84acfd4dffc601fd6f58a18c8
paulo <paulorgomes99@gmail.com>
2020-03-01 05:02:41 -0300

    Adicionando arquitetura

--- src/server.js
+++ src/server.js
@@ -3,8 +3,8 @@ import app from './app';
 const port = 3333;
 
 try {
-  app.listen(port);
-  console.log(`Aplicativo disponivel na porta ${port}`);
+    app.listen(port);
+    console.log(`Aplicativo disponivel na porta ${port}`);
 } catch (error) {
-  console.log(`Erro: ${error}`);
+    console.log(`Erro: ${error}`);
 }
