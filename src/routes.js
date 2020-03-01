773f04a86505d4724854cb3e28c789f0bb9b8c21
paulo <paulorgomes99@gmail.com>
2020-03-01 04:47:59 -0300

    Adicionando padronizadores automaticos no codigo

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,8 @@ import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get('/',(req,res)=> { 
-    return res.json({ message: 'Hello'});
+routes.get('/', (req, res) => {
+  return res.json({ message: 'Hello' });
 });
 
 export default routes;
--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
 import app from './app';
 
-const port = 3333
+const port = 3333;
 
 try {
-    app.listen(port);
-    console.log(`Aplicativo disponivel na porta ${port}`)
+  app.listen(port);
+  console.log(`Aplicativo disponivel na porta ${port}`);
 } catch (error) {
-    console.log(`Erro: ${error}`)
+  console.log(`Erro: ${error}`);
 }
